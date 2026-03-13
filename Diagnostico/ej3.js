fac793a290fba75aa210d1f5d094f04b01f4db87
Tiziano Pirez <tizippuden@gmail.com>
2026-03-13 11:50:02 -0300

    Add vowel checking functionality in ej3.js

---
+++ Diagnostico/ej3.js
@@ -0,0 +1,17 @@
+let letra = prompt("iingresa una letra:");
+if (
+  letra === "a" ||
+  letra === "e" ||
+  letra === "i" ||
+  letra === "o" ||
+  letra === "u" ||
+  letra === "A" ||
+  letra === "E" ||
+  letra === "I" ||
+  letra === "O" ||
+  letra === "U"
+) {
+  console.log("es una vocal");
+} else {
+  console.log("no es una vocal");
+}
