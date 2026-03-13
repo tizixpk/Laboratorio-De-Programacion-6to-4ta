030b2a5e5008198c8dd9bf32fb7857d54fe3e7dc
Tiziano Pirez <tizippuden@gmail.com>
2026-03-13 11:41:35 -0300

    Create ej1.js

---
+++ Diagnostico/ej1.js
@@ -0,0 +1,4 @@
+let Numero1 = prompt("Ingresa el primer num:"); 
+let Numero2 = prompt("Ingresa el seg num :");
+let suma = parseInt(Numero1) + parseInt(Numero2);
+console.log(`La suma es: ${suma}`); 
