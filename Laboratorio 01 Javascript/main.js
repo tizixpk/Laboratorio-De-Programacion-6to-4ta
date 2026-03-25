d5ee37aee9b54c521036247e7af3dd615d519215
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-03-25 17:24:54 -0300

    Create main.js with JavaScript exercises
    Add initial JavaScript exercises for practice.

---
+++ Laboratorio 01 Javascript/main.js
@@ -0,0 +1,35 @@
+/*
+Ejercicio 1:
+console.log("hola");
+
+Ejercicio 2:
+let numero = 15;
+let booleano = true;
+let underfined;
+let null1 = null;
+console.log("hola"/2);
+String(100);
+console.log(typeof numero);
+console.log(typeof booleano);
+console.log(typeof underfined);
+console.log(typeof null1);
+console.log(typeof String);
+
+Ejercicio 3:
+function sumar (a,b) {
+return a + b;
+}
+console.log(typeof sumar)
+
+Ejercicio 4:
+function sumar (a) {
+if (typeof(a) == "number" ) {
+     console.log(true);
+}
+else  {
+    console.log(false);
+}
+}
+let a = 5;
+sumar(a);
+*/
