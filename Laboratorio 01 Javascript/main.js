1836f103bc0eac9adcc3aab172ab4b60e7739abe
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-03-28 18:56:17 -0300

    Update main.js

--- Laboratorio 01 Javascript/main.js
+++ Laboratorio 01 Javascript/main.js
@@ -48,6 +48,35 @@ let v = [1, 2, 3];
 console.log(vec(v)); 
 
 Ejercicio 7:
+function convertirStringANumero(valor) { return Number(valor) }
+function convertirNumeroAString(valor) { return String(valor) }
+function convertirBooleanoAString(valor) { return String(valor) }
+function convertirStringABooleano(valor) { return valor.toLowerCase() === "true" }
+let string_ej = "123";
+let numero_ej = 456;
+let booleano_ej = true;
+let stringBooleano_ej = "true";
+let resultado1 = convertirStringANumero(string_ej);
+console.log("valor original:", string_ej);
+console.log("tipo original:", typeof string_ej);
+console.log("valor convertido:", resultado1);
+console.log("tipo convertido:", typeof resultado1);
+let resultado2 = convertirNumeroAString(numero_ej);
+console.log("valor original:", numero_ej);
+console.log("tipo original:", typeof numero_ej);
+console.log("valor convertido:", resultado2);
+console.log("tipo convertido:", typeof resultado2);
+let resultado3 = convertirBooleanoAString(booleano_ej);
+console.log("Valor original:", booleano_ej);
+console.log("Tipo original:", typeof booleano_ej);
+console.log("Valor convertido:", resultado3);
+console.log("Tipo convertido:", typeof resultado3);
+let resultado4 = convertirStringABooleano(stringBooleano_ej);
+console.log("Valor original:", stringBooleano_ej);
+console.log("Tipo original:", typeof stringBooleano_ej);
+console.log("Valor convertido:", resultado4);
+console.log("Tipo convertido:", typeof resultado4);
 
+Ejercicio 9:
 
 */
