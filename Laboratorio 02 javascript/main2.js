f5ef0392aeead66a8fcdd4586aa5284758282743
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-04-15 16:58:44 -0300

    Update main2.js

--- Laboratorio 02 javascript/main2.js
+++ Laboratorio 02 javascript/main2.js
@@ -18,15 +18,41 @@ function nombresProductos(lista_de_productos_ejemplo) {
 console.log(nombresProductos(productos));
 
 //Crear una función que retorne los productos de una categoría dada.
-function obtenerProductosPorCategoria(listaDeProductos, categoria) {
+function productosxcategoria(listaproductos, categoria) {
     let productoss = [];
     let j = 0;
-    for (let i = 0; i < listaDeProductos.length; i++) {
-        if (listaDeProductos[i].categoria === categoria) {
-            productoss[j] = listaDeProductos[i];
+    for (let i = 0; i < listaproductos.length; i++) {
+        if (listaproductos[i].categoria === categoria) {
+            productoss[j] = listaproductos[i];
             j++; 
         }
     }
     return productoss;
 }
-console.log(obtenerProductosPorCategoria(productos, "tecnologia"));
+console.log(productosxcategoria(productos, "tecnologia"));
+
+//Crear una función que retorne los productos cuyo stock sea igual a 0.
+function productossinvalor(productosss) {
+    let producto_sinstock = [];
+    let j = 0;
+    for (let i = 0; i < productos.length; i++) {
+        if (productosss[i].stock === 0) {
+            productos_sinstock[i] = productosss[i];
+            j++; 
+        }
+    }
+console.log(productossinvalor(productos));
+
+//Calcular el valor total del inventario usando la fórmula : precio * stock
+function valor_inventario(productosss2) {
+  productosss2 = [
+    { nombre: "Mouse", categoria: "tecnologia", precio: 1000, stock: 10 },  { nombre: "Teclado", categoria: "tecnologia", precio: 2000, stock: 5 } ];
+    let valor_inventario = 0;
+  for (let i = 0; i < productosss2.length; i++) {
+        valor_inventario+= (productosss[i].stock * productosss[i].precio)
+        }
+    return valor_inventario;
+    }
+console.log("valor inventario:", valor_inventario(productosss2));
+//Crear una función que retorne el objeto correspondiente al producto más caro.
+    
