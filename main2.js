246e87d0b543d81a1fbee1ad770f7dfafd9fac11
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-04-15 16:07:04 -0300

    Update and rename Laboratorio 02 Javascript to main2.js

---
+++ main2.js
@@ -0,0 +1,9 @@
+//Crear una función que reciba un array de productos y devuelva un nuevo array con solo nombres.
+function Nombres_productos (productos) {
+const productos = [
+    { nombre: "Mouse", categoria: "tecnologia", precio: 1000, stock: 10 },
+    { nombre: "Teclado", categoria: "tecnologia", precio: 2000, stock: 5 },
+    { nombre: "Monitor", categoria: "tecnologia", precio: 50000, stock: 0 },
+    { nombre: "Silla", categoria: "hogar", precio: 15000, stock: 2 }
+];
+}
