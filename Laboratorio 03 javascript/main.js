3277b7c5d28aaedf7206a340568c095d363fc86d
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-04-22 16:54:26 -0300

    Create main.js

---
+++ Laboratorio 03 javascript/main.js
@@ -0,0 +1,16 @@
+do {
+console.log ("----- menu ----- ");
+console.log ("1. Agregar producto ");
+console.log ("2. Eliminar producto ");
+console.log ("3. Modificar producto ");
+console.log ("4. mostrar producto y salir ");
+Opcion = parseInt(prompt("ingresa:"));
+switch (opcion) {
+    case 1:
+        producto.nombre = parseInt(prompt("ingresa el producto"));
+        producto.categoria = parseInt(prompt("ingresa su categoria"));
+        producto.precio = parseInt(prompt("ingresa el precio"));
+        producto.precio = parseInt(prompt("ingresa el stock"));
+        let obj
+}
+}while(Opcion!==4);
