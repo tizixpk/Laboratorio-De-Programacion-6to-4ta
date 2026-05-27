45f308688641a287c821dcc0fdf5d6a5a47e89b6
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-05-27 15:55:19 -0300

    Create main.js

---
+++ Laboratorio Gestion de productos 2/main.js
@@ -0,0 +1,6 @@
+const productos = [
+{nombre: "Mouse", categoria: "tecnologia", precio: 1000, stock: 10 },
+{nombre: "Teclado", categoria: "tecnologia", precio: 2000, stock: 5 },
+{nombre: "Monitor", categoria: "tecnologia", precio: 50000, stock: 0 },
+{nombre: "Silla", categoria: "hogar", precio: 15000, stock: 2 }
+];
