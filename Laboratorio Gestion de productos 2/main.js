2145e4f6f7078586682abb08d205fb30bb2294c4
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-06-03 15:58:01 -0300

    Update main.js

--- Laboratorio Gestion de productos 2/main.js
+++ Laboratorio Gestion de productos 2/main.js
@@ -1,224 +1,272 @@
-let productos = [
-  { nombre: "Mouse", categoria: "tecnologia", precio: 1000, stock: 10 },
-  { nombre: "Teclado", categoria: "tecnologia", precio: 2000, stock: 5 },
-  { nombre: "Monitor", categoria: "tecnologia", precio: 50000, stock: 0 },
-  { nombre: "Silla", categoria: "hogar", precio: 15000, stock: 2 }
+//===============================================================
+// Consigna: Sistema de Gestión de Productos
+// Alumno/a: Tiziano Pirez
+// Fecha: 03/06/2026
+// Descripción: menu con sub menu
+//===============================================================
+
+// array inicial 
+const productos = [
+    { nombre: "Mouse", categoria: "tecnologia", precio: 1000, stock: 10 },
+    { nombre: "Teclado", categoria: "tecnologia", precio: 2000, stock: 5 },
+    { nombre: "Monitor", categoria: "tecnologia", precio: 50000, stock: 0 },
+    { nombre: "Silla", categoria: "hogar", precio: 15000, stock: 2 }
 ];
 
-let productosActualizados = null;
+// Variable  q tendra la versión actualizada de los productos
+let productosActuales = productos;
+// Variable para finalizar el programa
+let salirPrograma = false;
 
-let salir = false;
+/*
+agrega un nuevo producto al array.
+Retorno: devuelve un nuevo array con el producto agregado.
+*/
+function agregarProducto(temp) {
+    let nombreProducto = prompt("Nombre del producto");
+    let categoriaProducto = prompt("Categoría");
+    let precioProducto = Number(prompt("Precio"));
+    let stockProducto = Number(prompt("Stock"));
+    let nuevoArray = [];
+    temp.forEach(function (producto) {
+        nuevoArray[nuevoArray.length] = {
+            nombre: producto.nombre,
+            categoria: producto.categoria,
+            precio: producto.precio,
+            stock: producto.stock
+        };
+    });
+    nuevoArray[nuevoArray.length] = {};
+    nuevoArray[nuevoArray.length - 1].nombre = nombreProducto;
+    nuevoArray[nuevoArray.length - 1].categoria = categoriaProducto;
+    nuevoArray[nuevoArray.length - 1].precio = precioProducto;
+    nuevoArray[nuevoArray.length - 1].stock = stockProducto;
+    console.log("producto agregado");
+    return nuevoArray;
+}
 
+/*
+elimina un producto según el índice ingresado.
+Retorno: devuelve un nuevo array sin el producto eliminado.
+*/
+function eliminarProducto(temp) {
+    let indice = Number(
+        prompt("Ingrese el índice del producto a eliminar")
+    );
+    let nuevoArray = [];
+    temp.forEach(function (producto, posicion) {
+        if (posicion !== indice) {
+            nuevoArray[nuevoArray.length] = producto;
+        }
+    });
+    console.log("pproducto eliminado");
+    return nuevoArray;
+}
+
+/*
+modifica un producto según el indice  puesto.
+retorno: devuelve el nuevo array actualizado.
+*/
+function modificarProducto(temp) {
+    let indice = Number(
+        prompt("Ingrese el índice del producto a modificar")
+    );
+    let nuevoArray = [];
+    temp.forEach(function (producto, posicion) {
+        if (posicion === indice) {
+            console.table([producto]);
+            nuevoArray[nuevoArray.length] = {
+                nombre: prompt("Nuevo nombre"),
+                categoria: prompt("Nueva categoría"),
+                precio: Number(prompt("Nuevo precio")),
+                stock: Number(prompt("Nuevo stock"))
+            };
+        } else {
+            nuevoArray[nuevoArray.length] = {
+                nombre: producto.nombre,
+                categoria: producto.categoria,
+                precio: producto.precio,
+                stock: producto.stock
+            };
+        }
+    });
+    console.log("Producto modificado");
+    return nuevoArray;
+}
+
+/*
+muestra todos los productos con stock igual a cero.
+no retorna valor.
+*/
+function mostrarSinStock(temp) {
+    let sinStock = [];
+    temp.forEach(function (producto) {
+        if (producto.stock === 0) {
+            sinStock[sinStock.length] = producto;
+        }
+    });
+    console.table(sinStock);
+}
+
+/*
+busca productos según la categoría ingresada.
+retorno: no retorna valor.
+*/
+function buscarPorCategoria(temp) {
+    let categoriaBuscada = prompt(
+        "ingrese una categoría"
+    );
+    let encontrados = [];
+    temp.forEach(function (producto) {
+        if (producto.categoria === categoriaBuscada) {
+            encontrados[encontrados.length] = producto;
+        }
+    });
+    console.table(encontrados);
+}
+
+/*
+genera un nuevo array con el precio actualizado
+de un producto específico.
+retorno: devuelve un nuevo array.
+*/
+function aplicarAumento(temp) {
+    let nombreBuscado = prompt(
+        "Ingrese el nombre del producto"
+    );
+    let porcentaje = Number(
+        prompt("Ingrese porcentaje de aumento")
+    );
+    let nuevoArray = [];
+    temp.forEach(function (producto) {
+        if (producto.nombre === nombreBuscado) {
+            nuevoArray[nuevoArray.length] = {
+                nombre: producto.nombre,
+                categoria: producto.categoria,
+                precio:
+                producto.precio + (producto.precio * porcentaje / 100),
+                stock: producto.stock
+            };
+
+        } else {
+            nuevoArray[nuevoArray.length] = {
+                nombre: producto.nombre,
+                categoria: producto.categoria,
+                precio: producto.precio,
+                stock: producto.stock
+            };
+        }
+    });
+    console.log("Aumento aplicado");
+    return nuevoArray;
+}
+
+/*
+muestra el producto con el mayor precio
+retorno: no retorna  nada
+*/
+function productoMasCaro(temp) {
+    let masCaro = temp[0];
+    temp.forEach(function (producto) {
+        if (producto.precio > masCaro.precio) {
+            masCaro = producto;
+        }
+    });
+    console.table([masCaro]);
+}
+
+/*
+muestra todos los productos
+retorno: no retorna valor
+*/
+function mostrarProductos(temp) {
+    console.table(temp);
+}
+
+/*
+muestra el submenú de opciones adicionales.
+retorno: devuelve el array actualizado.
+*/
+function menuExtra(temp) {
+    let opcionExtra;
+    do {
+        opcionExtra = prompt(
+            "SUBMENUU\n\n" +
+            "1-mostrar productos sin stock\n" +
+            "2-buscar productos x categoría\n" +
+            "3-aplicar aumento de precio x nombre\n" +
+            "4-mostrar producto mas caro\n" +
+            "5-volver al menu anterior"
+        );
+
+        switch (opcionExtra) {
+            case "1":
+                mostrarSinStock(temp);
+                salirPrograma = true;
+                break;
+
+            case "2":
+                buscarPorCategoria(temp);
+                salirPrograma = true;
+                break;
+
+            case "3":
+                temp = aplicarAumento(temp);
+                break;
+            case "4":
+                productoMasCaro(temp);
+                break;
+
+            case "5":
+                console.log("Volviendo al menú principal");
+                break;
+
+            default:
+                console.log("Opción inválida");
+        }
+
+    } while (opcionExtra !== "5" && !salirPrograma);
+    return temp;
+}
+
+// MENU PRINCIPAL
+let opcion;
 do {
+    opcion = prompt(
+        "MENU PRINCIPAL\n\n" +
+        "1-agregar producto\n" +
+        "2-eliminar producto\n" +
+        "3-modificar producto\n" +
+        "4-mas opciones\n" +
+        "5-mostrar productos y salir"
+    );
 
-  let opcion = Number(prompt(`
-1 - Agregar producto
-2 - Eliminar producto
-3 - Modificar producto
-4 - Más opciones
-5 - Mostrar productos y salir
-Ingrese una opción:
-`));
-
-  switch (opcion) {
-
-    case 1:
-
-      let nombre = prompt("Ingrese nombre del producto:");
-      let categoria = prompt("Ingrese categoría:");
-      let precio = Number(prompt("Ingrese precio:"));
-      let stock = Number(prompt("Ingrese stock:"));
-
-      productos.push({
-        nombre,
-        categoria,
-        precio,
-        stock
-      });
-
-      console.log("Producto agregado correctamente.");
-      break;
-
-    case 2:
-
-      let eliminar = prompt("Ingrese el nombre del producto a eliminar:");
-
-      productos = productos.filter(producto =>
-        producto.nombre.toLowerCase() !== eliminar.toLowerCase()
-      );
-
-      console.log("Producto eliminado correctamente.");
-      break;
-
-
-    case 3:
-
-      let modificar = prompt("Ingrese el nombre del producto a modificar:");
-
-      productos.forEach(producto => {
-
-        if (producto.nombre.toLowerCase() === modificar.toLowerCase()) {
-
-          producto.categoria = prompt("Nueva categoría:");
-          producto.precio = Number(prompt("Nuevo precio:"));
-          producto.stock = Number(prompt("Nuevo stock:"));
-
-          console.log("Producto modificado correctamente.");
-        }
-
-      });
-
-      break;
-
-
-    case 4:
-
-      let volver = false;
-
-      do {
-
-        let subOpcion = Number(prompt(`
-===== SUBMENÚ =====
-
-1 - Mostrar productos sin stock
-2 - Buscar productos por categoría
-3 - Aplicar aumento de precio por nombre
-4 - Mostrar producto más caro
-5 - Volver al menú anterior
-
-Ingrese una opción:
-`));
-
-        switch (subOpcion) {
-            
-          case 1:
-
-            let sinStock = [];
-
-            productos.forEach(producto => {
-
-              if (producto.stock === 0) {
-                sinStock.push(producto);
-              }
-
-            });
-
-            console.table(sinStock);
-
-            // Finaliza completamente
-            salir = true;
-            volver = true;
-
+    switch (opcion) {
+        case "1":
+            productosActuales = agregarProducto(productosActuales);
             break;
 
-          case 2:
-
-            let categoriaBuscada = prompt("Ingrese categoría:");
-
-            let encontrados = [];
-
-            productos.forEach(producto => {
-
-              if (
-                producto.categoria.toLowerCase() ===
-                categoriaBuscada.toLowerCase()
-              ) {
-                encontrados.push(producto);
-              }
-
-            });
-
-            console.table(encontrados);
-
-            salir = true;
-            volver = true;
-
+        case "2":
+            productosActuales = eliminarProducto(productosActuales);
             break;
 
-          case 3:
-
-            let nombreProducto = prompt("Ingrese nombre del producto:");
-            let porcentaje = Number(prompt("Ingrese porcentaje de aumento:"));
-
-            productosActualizados = [];
-
-            productos.forEach(producto => {
-
-              if (
-                producto.nombre.toLowerCase() ===
-                nombreProducto.toLowerCase()
-              ) {
-
-                let nuevoPrecio =
-                  producto.precio +
-                  (producto.precio * porcentaje / 100);
-
-                productosActualizados.push({
-                  ...producto,
-                  precio: nuevoPrecio
-                });
-
-              } else {
-
-                productosActualizados.push(producto);
-
-              }
-
-            });
-
-            console.table(productosActualizados);
-
+        case "3":
+            productosActuales = modificarProducto(productosActuales);
             break;
 
-          case 4:
-
-            let arrayUsado =
-              productosActualizados || productos;
-
-            let productoCaro = arrayUsado[0];
-
-            arrayUsado.forEach(producto => {
-
-              if (producto.precio > productoCaro.precio) {
-                productoCaro = producto;
-              }
-
-            });
-
-            console.table([productoCaro]);
-
+        case "4":
+            productosActuales = menuExtra(productosActuales);
             break;
 
-          case 5:
-
-            volver = true;
+        case "5":
+            mostrarProductos(productosActuales);
             break;
 
-          default:
-            console.log("Opción inválida.");
+        default:
 
-        }
+            console.log("Ingrese una opción");
+    }
 
-      } while (!volver);
+} while (opcion !== "5" && !salirPrograma);
 
-      break;
-
-    case 5:
-
-      console.log("===== LISTADO FINAL =====");
-
-      if (productosActualizados) {
-        console.table(productosActualizados);
-      } else {
-        console.table(productos);
-      }
-
-      salir = true;
-
-      break;
-
-    default:
-      console.log("Opción inválida.");
-  }
-
-} while (!salir);
+console.log("Programa finalizadooo");
