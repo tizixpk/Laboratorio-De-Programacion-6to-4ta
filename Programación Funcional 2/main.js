95327a3a9b1e51ddc64ec09b77f3fc3044904245
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-06-10 16:06:21 -0300

    Update main.js

--- Programación Funcional 2/main.js
+++ Programación Funcional 2/main.js
@@ -1,1 +1,9 @@
+//===============================================================
+// Consigna: Programacion Funcional 2
+// Alumno/a: Tiziano Pirez
+// Fecha: 10/06/2026
+//===============================================================
 
+// Una funcion pura es cuando das el mismo argumento y te tiene q dar siempre el mismo resultado
+// Un dato inmutable significa que el valor no puede ser modificado
+// Orden superior: Puede devolver o recibe una funcion
