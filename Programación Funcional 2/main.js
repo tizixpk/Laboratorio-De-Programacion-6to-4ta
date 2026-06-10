27fec6ab57a4c860e594d14652c8a7c4d13eaea3
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-06-10 16:59:40 -0300

    Update main.js

--- Programación Funcional 2/main.js
+++ Programación Funcional 2/main.js
@@ -9,14 +9,11 @@
 // Orden superior: Puede devolver o recibe una funcion
 // PARTE B: F1 no es pura ya que tiene efectos secundarios con contador, f2 es pura ya que si le das un valor siempre dara el mismo resultado, f3  no es pura ya que modifica lista, f4 no es pura ya que siempre da un valor distinto.
 //PARTE C:
-
 const f1 = (x, contador) => ({ 
   resultado: x * 2, 
   nuevoContador: contador + 1 
 });
-
 const f3 = (lista) => [...lista, 99]; 
-
 //PARTE B.2
 const precios = [1200, 800, 3500, 450, 2100, 990];
 const preciosConIVA = precios.map(x => {
@@ -45,3 +42,12 @@ if ( x < 100 ) {
 console.log(barato);
 console.log(precios);
 //PARTE B.3
+const productos = [
+  { nombre: 'Guitarra', categoria: 'Instrumento', precio: 15000 },
+  { nombre: 'Auriculares', categoria: 'Accesorio', precio: 3200 },
+  { nombre: 'Piano digital', categoria: 'Instrumento', precio: 42000 },
+  { nombre: 'Cable XLR', categoria: 'Accesorio', precio: 850 },
+  { nombre: 'Batería', categoria: 'Instrumento', precio: 68000 },
+  { nombre: 'Micrófono', categoria: 'Accesorio', precio: 7500 },
+];
+const soloInstrumentos = Productos.filter((productos2) => productos2.length > 6);
