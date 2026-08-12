86c74c45a2c215cb743eaf5909560a3b051db60d
𝑻𝒊𝒛𝒊𝒂𝒏𝒐 𝑷𝒊𝒓𝒆𝒛 <tizippuden@gmail.com>
2026-08-12 17:06:46 -0300

    Add files via upload

---
+++ Inicio de programacion Funcional/main.js
@@ -0,0 +1,13 @@
+//EJ 1
+const EJERCICIO1 = console.log(document.getElementById("parrafo1").textContent);
+//EJ 2
+const EJERCICIO2 = document.getElementsByClassName("clase1");
+console.log(EJERCICIO2);
+for (let index = 0; index < 3; index++) {
+    console.log(EJERCICIO2[index]);
+}
+//EJ 3
+const EJERCICIO3 = document.querySelector(".mostaza12");
+EJERCICIO3.src = "https://scontent.cdninstagram.com/v/t51.82787-19/656282702_17863076358666545_6017669883527419141_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_cat=108&ccb=7-5&_nc_sid=f7ccc5&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=Dm5B3OA6ueEQ7kNvwEy-hTU&_nc_oc=AdrCA-5sgjchb1cT63xc0EZxnOemdnasaAO0LQgoheTd3nEklf23ppSbq-rbRvy_Wi1G_uqQ4Lp49TD9ANvlexry&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=-KbCeIj7T4jMRl6Cp1UzUA&_nc_ss=7b689&oh=00_AQERfXYDbehGC-YAsBHfBApSke_LN5QDtfKpT6FttlEg4A&oe=6A82B35E";
+EJERCICIO3.alt = "mostaza" ;
+//EJ 4
